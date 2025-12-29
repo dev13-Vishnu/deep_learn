1693085631c252ce1e0324d3e55ec1bc86d49b66
Vishnu P S <vishnu13three@gmail.com>
2025-12-29 09:36:48 +0530

    feat(ui): add minimal signup page layout

---
+++ deep-learn-client/src/presentation/components/auth/SignupFom.tsx
@@ -0,0 +1,29 @@
+export default function SignupForm() {
+  return (
+    <div className="signup-form">
+      <h2>Create Your Account</h2>
+      <div className="row">
+        <input type="text" placeholder="First Name" />
+        <input type="text" placeholder="Last Name" />
+      </div>
+
+      <input type="email" placeholder="Email ID" />
+      <input type="tel" placeholder="Phone Number" />
+
+      <div className="row">
+        <input type="password" placeholder="Enter Password" />
+        <input type="password" placeholder="Confirm Password" />
+      </div>
+
+      <button className="primay-btn">Create Account</button>
+
+      <div className="divider">Sign up with</div>
+
+      <div className="social-buttons">
+        <button className="social facebook">Facebook</button>
+        <button className="social google">Google</button>
+        <button className="social microsoft">Microsoft</button>
+      </div>
+    </div>
+  );
+}
---
+++ deep-learn-client/src/presentation/pages/auth/Signup.tsx
@@ -0,0 +1,11 @@
+export default function Signup() {
+  return (
+    <div className="signup-container">
+      <div className="signup-left">
+        <img src="" alt="" className="signup-image" />
+      </div>
+
+      <div className="signup-right"></div>
+    </div>
+  );
+}
