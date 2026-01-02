c7e2c5208343dfadc3f721a4a77b362f396858ff
Vishnu P S <vishnu13three@gmail.com>
2026-01-02 23:50:09 +0530

    chore(ui): add tailwind v4 and align tokens to signup design

--- deep-learn-client/src/main.tsx
+++ deep-learn-client/src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './presentation/context/AuthProvider';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
