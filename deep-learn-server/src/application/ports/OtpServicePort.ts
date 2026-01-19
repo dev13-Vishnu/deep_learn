5322f197d47748890df09236dfc73b5473a07372
Vishnu P S <vishnu13three@gmail.com>
2026-01-19 21:31:15 +0530

    feat(application): normalize OTP service port

--- deep-learn-server/src/application/ports/OtpServicePort.ts
+++ deep-learn-server/src/application/ports/OtpServicePort.ts
@@ -1,6 +1,6 @@
-export type OtpPurpose = 'signup' | 'forgot-password'
+export type OtpPurpose = 'signup' | 'forgot-password';
 
 export interface OtpServicePort {
-    send(email: string, purpose: OtpPurpose): Promise <Date>;
-    verify(email: string, purpose: OtpPurpose, otp: string): Promise<void>;
+  requestOtp(email: string, purpose: OtpPurpose): Promise<void>;
+  verifyOtp(email: string, otp: string, purpose: OtpPurpose): Promise<void>;
 }
